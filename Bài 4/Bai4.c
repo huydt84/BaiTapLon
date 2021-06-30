251e7e639c4c5ed87c670447cd0501871eb72209
Đinh Trọng Huy <77562200+huydt84@users.noreply.github.com>
2021-06-30 20:48:53 +0700

    Update Bai4.c

--- Bài 4/Bai4.c
+++ Bài 4/Bai4.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-// #include<stdlib.h>
-//#include<time.h>
-//#include<windows.h>
+#include<stdlib.h>
+#include<time.h>
+#include<windows.h>
 
 void GoiTen(int n){
 	int m = n / 4;
